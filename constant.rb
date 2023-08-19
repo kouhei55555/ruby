320c5275e9cb57524f32e7407c34d0ef98a1c21c
kouhei55555 <141524423+kouhei55555@users.noreply.github.com>
2023-08-19 04:37:16 +0000

    Initial Commit

---
+++ constant.rb
@@ -0,0 +1,5 @@
+Pi = 3.14
+puts Pi
+
+Pi =　100
+puts Pi
---
+++ lesson6-1.rb
@@ -0,0 +1,9 @@
+puts "計算を始めます"
+puts "2つの値を入力してください"
+
+A = gets.to_i
+B = gets.to_i
+
+puts "計算結果を出力します"
+puts "#{A}*#{B}=#{A*B}"
+puts "計算を終了します"
